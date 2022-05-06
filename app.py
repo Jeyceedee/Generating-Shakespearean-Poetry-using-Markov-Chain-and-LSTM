155fdcf7f33a95157bf28bfabf684665f9ba80e9
Jeyceedee <70811916+Jeyceedee@users.noreply.github.com>
2022-05-06 20:18:57 +0800

    fixed failed to convert a Numpy array to a Tensor

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ with open('model/model.json', 'r') as json_file:
 
 app = Flask(__name__, static_url_path='/static')
 
-
+@app.route('/')
 @app.route("/home")
 def index():
     return render_template('index.html')
